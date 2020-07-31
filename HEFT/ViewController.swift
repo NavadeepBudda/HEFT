67a5514db276c8d72942da35aa45d2ee6b4cc6fe
theAmazingByte <ksamvid@gmail.com>
2020-07-31 15:49:56 -0400

    Removed Test Print

--- HEFT/ViewController.swift
+++ HEFT/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("I rock This was commited by Samvid")
+        
         
     }
 
