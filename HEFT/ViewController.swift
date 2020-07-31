5c23041888c2b8e382d5b40e2da40554ffbd3d62
Navadeep Budda <navadeep.budda@gmail.com>
2020-07-31 18:03:30 -0400

    Starting picker view

--- HEFT/ViewController.swift
+++ HEFT/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet var stateLabel: UILabel!
+    @IBOutlet var statePicker: UIPickerView!
+    
+    let states = ["Georgia","Florida","California"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
