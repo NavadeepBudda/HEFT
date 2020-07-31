d4d040d7c065b04283594f20bdd8ebdeb22cb374
Navadeep Budda <navadeep.budda@gmail.com>
2020-07-31 18:08:18 -0400

    Finished picker

--- HEFT/ViewController.swift
+++ HEFT/ViewController.swift
@@ -18,6 +18,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        stateLabel.text = states[row]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
